1c44e45528b076829c8282e9faacbbbd78a640a8
keshet-git <83539396+keshet-git@users.noreply.github.com>
2022-08-14 11:07:36 +0300

    Add files via upload

---
+++ 4 loop/index.js
@@ -0,0 +1,16 @@
+
+//for(let i = 1; i <= 10; i+=1){
+//  console.log(i);
+//}
+/*
+for(let i = 10; i > 0;  i-=2){
+  console.log(i);
+}
+
+console.log("Happy new year!");
+*/
+
+const nameTag = document.createElement("hi");
+nameTag.innerHTML = window.prompt("Enter your name");
+document.body.append(nameTag);
+
