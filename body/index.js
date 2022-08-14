b21aeb2329f2f8671270603658fb348b47e1cb36
keshet-git <83539396+keshet-git@users.noreply.github.com>
2022-08-14 11:43:57 +0300

    Add files via upload

---
+++ body/index.js
@@ -0,0 +1,7 @@
+let elemant = document.querySelector("#dissert");
+let children = Array.from(elemant.children);
+
+children.forEach(child => child.style.backgroundColor = "lightgreen");
+
+//firstElementChild
+//parentElement
