abd9716a5187bb68bbc7141d6389d705896ba325
keshet-git <83539396+keshet-git@users.noreply.github.com>
2022-08-14 11:45:28 +0300

    Add files via upload

---
+++ chek/index.js
@@ -0,0 +1,26 @@
+document.getElementById("myButton").onclick = function(){
+
+  const myCheckBox = document.getElementById("myCheckBox");
+  const visaBtn = document.getElementById("visaBtn");
+  const mastercardBtn = document.getElementById("mastercardBtn");
+  const paypalBtn = document.getElementById("paypalBtn");
+    if(myCheckBox.checked){
+      console.log("you are subscribed");
+    }
+    else{
+      console.log("you are NOT subscribed");
+    }
+
+    if(visaBtn.checked){
+      console.log("you are paying with a Visa!");
+    }
+    else if(mastercardBtn.checked){
+      console.log("you are paying with a Mastercard!");
+    }
+    else if(paypalBtn.checked){
+      console.log("you are paying with a paypalcard!");
+    }
+    else{
+      console.log("you must select a payment type")
+    }
+}
