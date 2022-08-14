3f25f833b826569bce13bde83dbe3f346c492ba4
keshet-git <83539396+keshet-git@users.noreply.github.com>
2022-08-14 11:42:32 +0300

    Add files via upload

---
+++ arrey/index.js
@@ -0,0 +1,11 @@
+let fruits = ["apple", "orange", "banana"];
+
+fruits.push("lemon");
+fruits.pop();
+fruits.unshift("mango");
+fruits.shift();
+
+let length = fruits.length;
+let index = fruits.indexOf("apple")
+
+console.log(index);
