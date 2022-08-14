51ea32e40e19af4833e52b11dd5bbe25d928c25c
keshet-git <83539396+keshet-git@users.noreply.github.com>
2022-08-14 11:50:39 +0300

    Add files via upload

---
+++ function/index.js
@@ -0,0 +1,17 @@
+startPrgram();
+
+function startPrgram(){
+  let userName = "keshet";
+  let age = 21;
+
+  happyBirthday(userName, age)
+}
+
+
+function happyBirthday(a, b){
+  console.log("Happy birthday to you!");
+  console.log("Happy birthday to you!");
+  console.log("Happy birthday dear", a);
+  console.log("Happy birthday to you!");
+  console.log("you are!", b, "year old!");
+}
