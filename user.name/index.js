38a8c62813ae9af9ea79bc1ba0b3c848aec1ce4a
keshet-git <83539396+keshet-git@users.noreply.github.com>
2022-08-14 12:15:51 +0300

    Add files via upload

---
+++ user.name/index.js
@@ -0,0 +1,15 @@
+let userName = "   Keshet Avrahami     ";
+let phoneNumber = "435-656-9987"
+/*
+console.log(userName.length);
+console.log(userName.charAt(0));
+console.log(userName.indexOf('e'));
+console.log(userName.lastIndexOf('e'));
+userName = userName.trim();
+userName = userName.toUpperCase();
+
+userName = userName.toLocaleLowerCase();
+*/
+phoneNumber = phoneNumber.replaceAll("-", "/");
+
+console.log(phoneNumber);
