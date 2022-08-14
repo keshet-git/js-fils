153de60225e3f16b01d421a4c2a8a0aec384fc5f
keshet-git <83539396+keshet-git@users.noreply.github.com>
2022-08-14 12:03:48 +0300

    Add files via upload

---
+++ nested function/index.js
@@ -0,0 +1,20 @@
+
+
+let userName = "keshet";
+let userInbox = 0;
+
+login();
+
+
+function login(){
+  displayUserName();
+  displayUserInbox();
+
+  function displayUserName(){
+    console.log(`Welcome ${userName}`);
+  }
+  function displayUserInbox(){
+    console.log(`You have ${userInbox} new messages`);
+  }
+}
+
