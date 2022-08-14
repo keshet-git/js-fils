bd886855cab615601b15857346c55f056366c8fe
keshet-git <83539396+keshet-git@users.noreply.github.com>
2022-08-14 12:02:59 +0300

    Add files via upload

---
+++ myTitle/insex.js
@@ -0,0 +1,8 @@
+const title = document.getElementById("myTitle");
+
+title.style.backgroundColor = "rgb(50, 200, 250)";
+title.style.color = "black";
+title.style.fontFamily = "consolas";
+title.style.textAlign = "centsr";
+title.style.border = "2px solid";
+title.style.display = "block"
