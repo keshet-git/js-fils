bc02fbb844043ec367f49b1cb44abf3efb03785e
keshet-git <83539396+keshet-git@users.noreply.github.com>
2022-08-14 11:48:45 +0300

    Add files via upload

---
+++ DOM/index.js
@@ -0,0 +1,10 @@
+
+//console.log(document);
+//console.log(document.title);
+//console.log(document.URL);
+
+//document.title = "Title go here!";
+//document.location = "http://www.google.com"
+
+document.body.style.backgroundColor = "skyblue";
+document.getElementById("myDiv").innerHTML = "SCOOBY DOO!";
