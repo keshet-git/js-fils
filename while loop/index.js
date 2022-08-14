68b10d5d4e3129fdbe6059f06f967c63430e8561
keshet-git <83539396+keshet-git@users.noreply.github.com>
2022-08-14 12:16:15 +0300

    Add files via upload

---
+++ while loop/index.js
@@ -0,0 +1,13 @@
+/*
+let userName = "";
+
+while(userName == "" || userName == null){
+  userName = window.prompt("Enter your name");
+}
+
+console.log("Hello", userName);
+*/
+
+while(1 == 1){
+  console.log("help i stuck in an infinite loop");
+}
