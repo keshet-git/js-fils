19ea56906e19b671923d94cbe7fd25d59de4116d
keshet-git <83539396+keshet-git@users.noreply.github.com>
2022-08-14 12:12:35 +0300

    Add files via upload

---
+++ temp2/index.js
@@ -0,0 +1,16 @@
+let temp = 15;
+let sunny = false;
+
+if(!(temp > 0)){
+  console.log("it's cold outside");
+}
+else{
+  console.log("it's warm outside");
+}
+
+if(!sunny){
+  console.log("it's cloudy uotside");
+}
+else{
+console.log("it's sunny uotside");
+}
