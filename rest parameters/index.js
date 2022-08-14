039fc72aa9f0812873cc1d89857fd16571d8bb7e
keshet-git <83539396+keshet-git@users.noreply.github.com>
2022-08-14 12:06:34 +0300

    Add files via upload

---
+++ rest parameters/index.js
@@ -0,0 +1,18 @@
+
+
+
+let a = 1;
+let b = 2;
+let c = 3;
+let d = 4;
+let e = 5;
+
+console.log(sum(a, b, d));
+
+function sum(... numbers){
+  let total = 0;
+  for(let number of numbers){
+    total += number
+  }
+  return total;
+}
