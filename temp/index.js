98c3ee5687f41ce743d42250921f5dd9f15cee46
keshet-git <83539396+keshet-git@users.noreply.github.com>
2022-08-14 12:11:30 +0300

    Add files via upload

---
+++ temp/index.js
@@ -0,0 +1,21 @@
+
+let temp = 15;
+let sunny = false;
+
+
+if(temp > 0 && temp < 30 && sunny){
+  console.log('the weather is good!');
+}
+else{
+  console.log('the weather is bad!');
+}
+/*
+let temp = 15;
+
+if(temp <=  0 || temp >=30){
+  console.log('the weather is bad!');
+}
+else{
+  console.log('the weather is good!');
+}
+*/
