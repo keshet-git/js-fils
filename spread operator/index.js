708017bec451480edeeb4bfb90431d2966eb499c
keshet-git <83539396+keshet-git@users.noreply.github.com>
2022-08-14 12:10:01 +0300

    Add files via upload

---
+++ spread operator/index.js
@@ -0,0 +1,14 @@
+
+//let userName = "keshet avrahami";
+let numbers = [10, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+//console.log(...userName);
+//let maximum = Math.max(...numbers);
+//console.log(maximum);
+
+let class1 =["Spongobob", "Patrick", "Sandy"];
+let class2 = ["Rooz", "jo", "mishel"];
+
+class1.push(...class2);
+
+console.log(...class1);
