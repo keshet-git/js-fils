02db026e177538b384ac8ca2f49d0f6125f8a26f
keshet-git <83539396+keshet-git@users.noreply.github.com>
2022-08-14 11:52:07 +0300

    Add files via upload

---
+++ if/index.js
@@ -0,0 +1,25 @@
+/*
+let age = 65;
+
+if(age >= 65){
+  console.log("you are a senior citizen!");
+}
+else if(age >= 18){
+  console.log("you are an adolt!");
+}
+else if(age < 0){
+  console.log("you have't been born yet!");
+}
+else{
+  console.log("you are a child!");
+}
+*/
+
+let online = false
+
+if(online){
+  console.log("you are online");
+}
+else{
+  console.log("you age offline!")
+}
