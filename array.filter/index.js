205a1ca37c5b23d7c50ddc5211b2ff24b955e225
keshet-git <83539396+keshet-git@users.noreply.github.com>
2022-08-14 11:08:56 +0300

    Add files via upload

---
+++ array.filter/index.js
@@ -0,0 +1,47 @@
+
+/*
+let ages = ["16", "18", "17", "20", "70"];
+let adulrs = ages.filter(checkAge);
+
+adulrs.forEach(print);
+
+function checkAge(element){
+  return element >= 18;
+}
+function print(element){
+  console.log(element);
+}
+
+
+let prices = [5, 8, 20, 35, 25];
+let total = prices.reduce(checkOut);
+
+console.log(`the total uus ${total}`);
+function checkOut(total, element){
+  return total + element;
+}
+
+
+let grades = [100, 60, 70, 80, 87, 96];
+
+grades = grades.sort(ascendingSort);
+
+grades.forEach(print);
+
+function descendingSort(x, y){
+  return y - x;
+}
+function ascendingSort(x, y){
+  return x -y;
+}
+function print(element){
+  console.log(element);
+}
+*/
+
+//const greeting = function(){
+  //console.log("Hello!");
+//}
+
+//greeting();
+
