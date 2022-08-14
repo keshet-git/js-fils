cafdea4af26389e87294fde105375b06a284c277
keshet-git <83539396+keshet-git@users.noreply.github.com>
2022-08-14 12:09:29 +0300

    Add files via upload

---
+++ snek/indx.js
@@ -0,0 +1,92 @@
+const gameBoard = document.querySelector("#gameBoard");
+const ctx = gameBoard.getContext("2d");
+const scoreText = document.querySelector("#scoreText");
+const resetBtn = document.querySelector("#resetBtn");
+const gameWidth = gameBoard.width;
+const gameHeight = gameBoard.hight;
+const boardBackground = "white";
+const snakeColor="lightgreen";
+const snakeBorder = "black";
+const foodColor = "red";
+const unitSize = 25;
+let running = false;
+let xVelocity = unitSize;
+let yVelocity = 0;
+let foodX;
+let foodY;
+let score = 0;
+let snake = [
+  {x:unitSize * 4, y:0},
+  {x:unitSize * 3, y:0},
+  {x:unitSize * 2, y:0},
+  {x:unitSize, y:0},
+  {x:0, y:0}
+];
+
+window.addEventListener("keydown", chengeDirection);
+resetBtn.addEventListener("click", resetGame);
+
+gameStart();
+
+function gameStart(){
+  running = true;
+  scoreText.textContent = score;
+  createFood();
+  drawFood();
+  nextTick();
+};
+function nextTick(){
+  if(running){
+    setTimeout(()=>{
+      clearBoard();
+      drawFood();
+      moveSnake();
+      drawSnake();
+      checkGameOver();
+      nextTick();
+    }, 80);
+  }
+  else{
+    displayGameOver();
+  }
+};
+function clearBoard(){
+  ctx.fillStyle = boardBackground;
+  ctx.fillRect(0, 0, gameWidth, gameHeight);
+};
+function createFood(){
+  function randomFood(min, max){
+    const ranNum = Math.round((Math.random() * (max - min) + min) / unitSize) * unitSize;
+    return ranNum;
+   }
+   foodX = randomFood(0, gameWidth - unitSize);
+   foodY = randomFood(0, gameWidth - unitSize);
+};
+function drawFood(){
+  ctx.fillStyle = foodColor;
+  ctx.fillRect(foodX, foodY, unitSize, unitSize);
+};
+function moveSnake(){
+  const head = {x: snake[0].x + xVelocity,
+                y: snake[0].y + yVelocity};
+
+  snake.unshift(head);
+    if(false){
+
+    }
+    else{
+      snake.pop();
+    }
+};
+function drawSnake(){
+  ctx.fillStyle = snakeColor;
+  ctx.strokeStyle = snakeBorder;
+  snake.forEach(snakePart => {
+    ctx.fillRect(snakePart.x, snakePart.y, unitSize, unitSize);
+    ctx.strokeRect(snakePart.x, snakePart.y, unitSize, unitSize);
+  })
+};
+function chengeDirection(){};
+function checkGameOver(){};
+function displayGameOver(){};
+function resetGame(){};
